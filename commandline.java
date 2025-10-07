62e07aef2d3038b888b10e32b02922b05b80d8d3
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-10-07 11:39:46 +0530

    Create commandline.java

---
+++ commandline.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args){
+      //string args kyu use karte hai 
+      // when we have to pass the argumern while runnin the programm 
+      //when can access through the indexs 
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
