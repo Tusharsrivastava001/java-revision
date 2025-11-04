26611b26d16c50e15e77f626e64f3f108cfce8eb
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-11-04 06:36:24 +0530

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -21,3 +21,28 @@ class Main{
         System.out.println(map.get(1));
     }
 }
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+        String str=sc.nextLine();
+        String[] words=str.split(" ");
+        HashMap<String,Integer> map=new HashMap<>();
+        for(String w : words){
+            map.put(w,map.getOrDefault(w,0)+1);
+        }
+        
+        System.out.println(map);
+        // HashMap<Integer,String> map=new HashMap<>();
+        // map.put(1,"Tushar");
+        // map.put(2,"aman");
+        // map.put(3,"Tushar");
+        
+        // System.out.println(map);
+        // System.out.println(map.get(1));
+        // System.out.println(map.keySet());
+        // System.out.println(map.get(1));
+    }
+}
