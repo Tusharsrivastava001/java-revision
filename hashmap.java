7921f4103f23e55d1d49d713628015d39cb95407
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-11-04 06:16:03 +0530

    Add Java program with TreeSet and HashMap usage
    Implement a simple Java program using TreeSet and HashMap to demonstrate basic operations.

---
+++ hashmap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        TreeSet<Integer> set=new TreeSet<>();
+        set.add(10);
+        set.add(20);
+        
+        for(int ele : set){
+            System.out.println(ele+" ");
+        }
+        
+        HashMap<Integer,String> map=new HashMap<>();
+        map.put(1,"Tushar");
+        map.put(2,"aman");
+        map.put(3,"Tushar");
+        
+        System.out.println(map);
+        System.out.println(map.get(1));
+        System.out.println(map.keySet());
+        System.out.println(map.get(1));
+    }
+}
